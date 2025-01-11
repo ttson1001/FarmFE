0460e639bc0b71b227fe9c49f85379b4384dd633
Thanh Son <ttson1001@gmail.com>
2025-01-12 00:26:28 +0700

    Okay

--- src/client/verify/error.tsx
+++ src/client/verify/error.tsx
@@ -1,6 +1,6 @@
 import { Card } from "primereact/card";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import erorr from "../../assets/error.png";
 
 const Error = () => {
@@ -23,15 +23,23 @@ const Error = () => {
     <>
       <div className="grid grid-cols-2 h-screen">
         <div className="col-span-full bg-purple-200 h-full justify-center flex items-center">
-          <Card className="w-2/3 h-2/3 ">
+          <Card className="w-2/3 h-auto ">
             <div className="justify-center flex items-center">
-              <img className="w-1/3 h-1/3" src={erorr} alt="" />
+              <img
+                className="md:w-1/3 md:h-1/3 sm:w-[250px] sm:h-[250px]"
+                src={erorr}
+                alt=""
+              />
             </div>
             <div className="text-center mt-4">
               <p className="text-lg font-medium">
                 Bạn sẽ được chuyển hướng về trang đăng nhập sau {countdown}{" "}
                 giây...
               </p>
+              Hoặc
+              <div className="font-bold">
+                <Link to={"/"}>Đến trang đăng nhập</Link>
+              </div>
             </div>
           </Card>
         </div>
--- src/client/verify/success.tsx
+++ src/client/verify/success.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"; // Hoặc thư viện định tuyến bạn sử dụng
+import { Link, useNavigate } from "react-router-dom"; // Hoặc thư viện định tuyến bạn sử dụng
 import { Card } from "primereact/card";
 import success from "../../assets/success.png";
 
@@ -23,15 +23,23 @@ const Success = () => {
   return (
     <div className="grid grid-cols-2 h-screen">
       <div className="col-span-full bg-purple-200 h-full justify-center flex items-center">
-        <Card className="w-2/3 h-2/3">
+        <Card className="w-2/3 h-auto">
           <div className="justify-center flex items-center">
-            <img className="w-1/3 h-1/3" src={success} alt="Success" />
+            <img
+              className="md:w- md:h-1/3 sm:w-[250px] sm:h-[250px]"
+              src={success}
+              alt="Success"
+            />
           </div>
           <div className="text-center mt-4">
             <p className="text-lg font-medium">
               Bạn sẽ được chuyển hướng về trang đăng nhập sau {countdown}{" "}
               giây...
             </p>
+            Hoặc
+            <div className="font-bold">
+              <Link to={"/"}>Đến trang đăng nhập</Link>
+            </div>
           </div>
         </Card>
       </div>
